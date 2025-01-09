f3fabb9aecea618cabbe6d07983af1fc04db41ba
Lucas <lucas.viaene@student.howest.be>
2025-01-09 13:25:11 +0000

    revert last commit

--- Models/Reservation.cs
+++ Models/Reservation.cs
@@ -4,6 +4,7 @@
     /// Represents a reservation made by a customer for an accommodation, including customer details, accommodation details,
     /// check-in and check-out dates, and the reservation status.
     /// </summary>
+    [Serializable]
     public class Reservation
     {
         private int Id;
